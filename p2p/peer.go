5415dc240371c3afee705ce6f04bae7ef50f9aa1
w3liu <w3liu@qq.com>
2020-07-28 17:36:34 +0800

    新增 测试用例

---
+++ p2p/peer.go
@@ -0,0 +1,30 @@
+package p2p
+
+import "net"
+
+type peerConn struct {
+	outbound   bool
+	conn       net.Conn
+	socketAddr *NetAddress
+	ip         net.IP
+}
+
+func newPeerConn(
+	outbound bool,
+	conn net.Conn,
+	socketAddr *NetAddress,
+) peerConn {
+
+	return peerConn{
+		outbound:   outbound,
+		conn:       conn,
+		socketAddr: socketAddr,
+	}
+}
+
+func (pc peerConn) ID() ID {
+	return ""
+}
+
+type peer struct {
+}
